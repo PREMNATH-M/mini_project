d156611c4c281c63e6273b405c0f4c14e7073716
PREMNATH-M <127302046+PREMNATH-M@users.noreply.github.com>
2023-08-01 15:38:34 +0530

    mini_project
    Railways_reservation

---
+++ Rail/src/dao/ReservationDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import model.Reservation;
+import java.util.List;
+
+public interface ReservationDao {
+    void create(Reservation reservation);
+    List<Reservation> read();
+    void update(Reservation reservation);
+    void delete(int reservationId);
+    Reservation readById(int reservationId);
+}
---
+++ Rail/src/util/DBHandler.java
@@ -0,0 +1,22 @@
+ package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBHandler {
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/railway_reservation", "root", "Vishnu@93");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return connection;
+    }
+}
